2bd69b95b4f3df400384d5bd405413605b01a1c7
swwwjjw <zhukovskiy.ev2@gmail.com>
2025-11-24 11:15:36 +0300

    Moved chart.js from web to project folder

--- backend/main.py
+++ backend/main.py
@@ -417,7 +417,7 @@ async def dashboard():
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Job Analytics Dashboard</title>
-  <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+  <script src="../frontend/chart.js"></script>
   <script src="../frontend/wordcloud2.js"></script>
   <style>
     body { 
